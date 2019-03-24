7c1ebaceaf8d070f404091ccbeeeb54e5754f53d
Joshua Williams <josh-131313@hotmail.com>
2019-03-24 08:35:45 -0700

    Update SetRegisterEqualConst.java

--- src/main/java/fortville/opcodes/SetRegisterEqualConst.java
+++ src/main/java/fortville/opcodes/SetRegisterEqualConst.java
@@ -11,6 +11,12 @@ import fortville.components.Registers;
 public class SetRegisterEqualConst implements Opcode{
     @Override
     public void execute(short data1, short data2, short data3, Memory memory, Display display, Registers registers){
-        
+        /*6xkk - LD Vx, byte
+		Set Vx = kk.
+		The interpreter puts the value kk into register Vx.*/
+		registers.storeRegister(data1, (byte) data2);
+
+		//Do we want incrementPC in opcode execution?
+		registers.incrementPC();
     }
 }
