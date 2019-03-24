a9b3bc1756e90ed1e231a3e32e422e4744457c8e
Joshua Williams <josh-131313@hotmail.com>
2019-03-24 09:22:53 -0700

    Update SkipNextInstructionNotEqualConst.java

--- src/main/java/fortville/opcodes/SkipNextInstructionNotEqualConst.java
+++ src/main/java/fortville/opcodes/SkipNextInstructionNotEqualConst.java
@@ -11,6 +11,16 @@ import fortville.components.Registers;
 public class SkipNextInstructionNotEqualConst implements Opcode{
     @Override
     public void execute(short data1, short data2, short data3, Memory memory, Display display, Registers registers){
-        
+        /*4xkk - SNE Vx, byte
+		Skip next instruction if Vx != kk.
+		The interpreter compares register Vx to kk, and if they are not equal, increments the program counter by 2.*/
+		Byte a = registers.loadRegister(data1);
+		Byte b = (byte) data2;
+		if(a != b){
+			registers.incrementPC();
+		}
+
+		//Do we want incrementPC in opcode execution?
+		registers.incrementPC();
     }
 }
