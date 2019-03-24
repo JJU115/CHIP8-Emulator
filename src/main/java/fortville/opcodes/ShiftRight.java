30ee8cd6840acdc1cb0a3d9fdf5c7ac382242c4f
Joshua Williams <josh-131313@hotmail.com>
2019-03-24 08:31:04 -0700

    Update ShiftRight.java

--- src/main/java/fortville/opcodes/ShiftRight.java
+++ src/main/java/fortville/opcodes/ShiftRight.java
@@ -11,6 +11,19 @@ import fortville.components.Registers;
 public class ShiftRight implements Opcode{
     @Override
     public void execute(short data1, short data2, short data3, Memory memory, Display display, Registers registers){
-        
+        /*8xy6 - SHR Vx {, Vy}
+		Set Vx = Vx SHR 1.
+		If the least-significant bit of Vx is 1, then VF is set to 1, otherwise 0. Then Vx is divided by 2.*/
+		int value = registers.loadRegister(data1);
+		if((value & 0x01)==1){
+			registers.storeRegister((short) 15, (byte) 1);
+		}else{
+			registers.storeRegister((short) 15, (byte) 0);
+		}
+		value = value >> 1;
+		registers.storeRegister(data1, (byte) value);
+
+		//Do we want incrementPC in opcode execution?
+		registers.incrementPC();
     }
 }
