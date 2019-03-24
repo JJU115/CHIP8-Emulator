a7c4269b584999a3556ad1a59f4d9fa59e2be95f
Joshua Williams <josh-131313@hotmail.com>
2019-03-24 08:39:40 -0700

    Update SetRegisterToDelayTimer.java

--- src/main/java/fortville/opcodes/SetRegisterToDelayTimer.java
+++ src/main/java/fortville/opcodes/SetRegisterToDelayTimer.java
@@ -11,6 +11,12 @@ import fortville.components.Registers;
 public class SetRegisterToDelayTimer implements Opcode{
     @Override
     public void execute(short data1, short data2, short data3, Memory memory, Display display, Registers registers){
-        
+        /*Fx07 - LD Vx, DT
+		Set Vx = delay timer value.
+		The value of DT is placed into Vx.*/
+		registers.storeRegister(data1, registers.getDelayTimer());
+
+		//Do we want incrementPC in opcode execution?
+		registers.incrementPC();
     }
 }
