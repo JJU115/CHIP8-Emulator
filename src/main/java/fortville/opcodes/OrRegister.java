a16b5f49698f1ff8514618b2b2028ee70e090e36
Joshua Williams <josh-131313@hotmail.com>
2019-03-23 23:00:05 -0700

    Update OrRegister.java

--- src/main/java/fortville/opcodes/OrRegister.java
+++ src/main/java/fortville/opcodes/OrRegister.java
@@ -11,6 +11,16 @@ import fortville.components.Registers;
 public class OrRegister implements Opcode{
     @Override
     public void execute(short data1, short data2, short data3, Memory memory, Display display, Registers registers){
-        
+        /*8xy1 - OR Vx, Vy
+		Set Vx = Vx OR Vy.
+		Performs a bitwise OR on the values of Vx and Vy, then stores the result in Vx. 
+		A bitwise OR compares the corrseponding bits from two values, and if either bit is 1, 
+		then the same bit in the result is also 1. Otherwise, it is 0.*/
+
+		int result = (registers.loadRegister(data1) | registers.loadRegister(data2));
+		registers.storeRegister(data1, (byte) result);
+
+		//Do we want incrementPC in opcode execution?
+		registers.incrementPC();
     }
 }
