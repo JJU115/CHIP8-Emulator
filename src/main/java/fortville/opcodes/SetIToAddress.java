ff041e32925974426cffdda306f7531b47a5a9c6
Joshua Williams <josh-131313@hotmail.com>
2019-03-23 23:40:28 -0700

    Update SetIToAddress.java

--- src/main/java/fortville/opcodes/SetIToAddress.java
+++ src/main/java/fortville/opcodes/SetIToAddress.java
@@ -11,6 +11,13 @@ import fortville.components.Registers;
 public class SetIToAddress implements Opcode{
     @Override
     public void execute(short data1, short data2, short data3, Memory memory, Display display, Registers registers){
-        
+        /*Annn - LD I, addr
+		Set I = nnn.
+		The value of register I is set to nnn.*/
+		
+		registers.storeI(data1);
+
+		//Do we want incrementPC in opcode execution?
+		registers.incrementPC();
     }
 }
