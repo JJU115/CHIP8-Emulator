dde495282e0b1bb46578ab17a3983cf264b2eae5
Joshua Williams <josh-131313@hotmail.com>
2019-03-23 23:35:13 -0700

    Update SetDelayTimer.java

--- src/main/java/fortville/opcodes/SetDelayTimer.java
+++ src/main/java/fortville/opcodes/SetDelayTimer.java
@@ -11,6 +11,12 @@ import fortville.components.Registers;
 public class SetDelayTimer implements Opcode{
     @Override
     public void execute(short data1, short data2, short data3, Memory memory, Display display, Registers registers){
-        
+        /*Fx15 - LD DT, Vx
+		Set delay timer = Vx.
+		DT is set equal to the value of Vx.*/
+		registers.setDelayTimer(registers.loadRegister(data1));
+
+		//Do we want incrementPC in opcode execution?
+		registers.incrementPC();
     }
 }
