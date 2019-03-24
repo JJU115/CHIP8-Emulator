9744e51a17b2d41cf9c2e90123e3060e707354f2
Joshua Williams <josh-131313@hotmail.com>
2019-03-23 22:53:33 -0700

    Update JumpToAddressPlusRegister.java

--- src/main/java/fortville/opcodes/JumpToAddressPlusRegister.java
+++ src/main/java/fortville/opcodes/JumpToAddressPlusRegister.java
@@ -11,6 +11,9 @@ import fortville.components.Registers;
 public class JumpToAddressPlusRegister implements Opcode{
     @Override
     public void execute(short data1, short data2, short data3, Memory memory, Display display, Registers registers){
-        
+        /*Bnnn - JP V0, addr
+		Jump to location nnn + V0.
+		The program counter is set to nnn plus the value of V0.*/
+		registers.setPC( (short) (data1 + (0xff & registers.loadRegister((short)0))));
     }
 }
