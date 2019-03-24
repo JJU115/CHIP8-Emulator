78163dad1f68c8dd42ebbc31bd5cade28af5bfac
Joshua Williams <josh-131313@hotmail.com>
2019-03-24 09:01:10 -0700

    Update SetRegisterToRegister.java

--- src/main/java/fortville/opcodes/SetRegisterToRegister.java
+++ src/main/java/fortville/opcodes/SetRegisterToRegister.java
@@ -11,6 +11,12 @@ import fortville.components.Registers;
 public class SetRegisterToRegister implements Opcode{
     @Override
     public void execute(short data1, short data2, short data3, Memory memory, Display display, Registers registers){
-        
+        /*8xy0 - LD Vx, Vy
+		Set Vx = Vy.
+		Stores the value of register Vy in register Vx.*/
+		registers.storeRegister(data1, registers.loadRegister(data2));
+
+		//Do we want incrementPC in opcode execution?
+		registers.incrementPC();
     }
 }
