048f795201e1e77e2acbd4b3cd12b61019d4b33a
Joshua Williams <josh-131313@hotmail.com>
2019-03-23 23:08:53 -0700

    Update ReturnSubroutine.java

--- src/main/java/fortville/opcodes/ReturnSubroutine.java
+++ src/main/java/fortville/opcodes/ReturnSubroutine.java
@@ -11,6 +11,9 @@ import fortville.components.Registers;
 public class ReturnSubroutine implements Opcode{
     @Override
     public void execute(short data1, short data2, short data3, Memory memory, Display display, Registers registers){
-        
+        /*00EE - RET
+		Return from a subroutine.
+		The interpreter sets the program counter to the address at the top of the stack, then subtracts 1 from the stack pointer.*/
+		registers.setPC(registers.loadStack());
     }
 }
