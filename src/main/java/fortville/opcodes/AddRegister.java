21ba246082e7431a24721ed266860fb22f19c965
Joshua Williams <josh-131313@hotmail.com>
2019-03-23 21:15:53 -0700

    Update AddRegister.java

--- src/main/java/fortville/opcodes/AddRegister.java
+++ src/main/java/fortville/opcodes/AddRegister.java
@@ -11,6 +11,23 @@ import fortville.components.Registers;
 public class AddRegister implements Opcode{
     @Override
     public void execute(short data1, short data2, short data3, Memory memory, Display display, Registers registers){
-        
+        /*8xy4 - ADD Vx, Vy
+		Set Vx = Vx + Vy, set VF = carry.
+		The values of Vx and Vy are added together. If the result is greater than 8 bits (i.e., > 255,) 
+		VF is set to 1, otherwise 0. Only the lowest 8 bits of the result are kept, and stored in Vx.*/
+		int VX = (registers.loadRegister(data1) & 0xff);
+		int VY = (registers.loadRegister(data2) & 0xff);
+		int sum = VX + VY;
+
+		if(sum>=256){
+			registers.storeRegister((short) 15, (byte) 1);
+			sum -= 256;
+		}else{
+			registers.storeRegister((short) 15, (byte) 0);
+		}
+		registers.storeRegister(data1, (byte) sum);
+
+		//Do we want incrementPC in opcode execution?
+		registers.incrementPC();
     }
 }
