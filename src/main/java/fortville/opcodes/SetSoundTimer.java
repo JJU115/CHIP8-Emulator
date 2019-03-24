4fcd09186be6e5491dff5681dc9e3ad3149153be
Joshua Williams <josh-131313@hotmail.com>
2019-03-24 09:05:37 -0700

    Update SetSoundTimer.java

--- src/main/java/fortville/opcodes/SetSoundTimer.java
+++ src/main/java/fortville/opcodes/SetSoundTimer.java
@@ -11,6 +11,12 @@ import fortville.components.Registers;
 public class SetSoundTimer implements Opcode{
     @Override
     public void execute(short data1, short data2, short data3, Memory memory, Display display, Registers registers){
-        
+        /*Fx18 - LD ST, Vx
+		Set sound timer = Vx.
+		ST is set equal to the value of Vx.*/
+		registers.setSoundTimer(registers.loadRegister(data1));
+
+		//Do we want incrementPC in opcode execution?
+		registers.incrementPC();
     }
 }
