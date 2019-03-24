60f8916e413ee509a802012cd43bc35de28272c0
Joshua Williams <josh-131313@hotmail.com>
2019-03-24 08:58:21 -0700

    Update SetRegisterToRand.java

--- src/main/java/fortville/opcodes/SetRegisterToRand.java
+++ src/main/java/fortville/opcodes/SetRegisterToRand.java
@@ -11,6 +11,14 @@ import fortville.components.Registers;
 public class SetRegisterToRand implements Opcode{
     @Override
     public void execute(short data1, short data2, short data3, Memory memory, Display display, Registers registers){
-        
+        /*Cxkk - RND Vx, byte
+		Set Vx = random byte AND kk.
+		The interpreter generates a random number from 0 to 255, which is then ANDed with the value kk.
+		The results are stored in Vx. See instruction 8xy2 for more information on AND.*/
+		Random rand = new Random();
+		registers.storeRegister(data1, (byte) ((rand.nextInt(256)) & data2));
+
+		//Do we want incrementPC in opcode execution?
+		registers.incrementPC();
     }
 }
