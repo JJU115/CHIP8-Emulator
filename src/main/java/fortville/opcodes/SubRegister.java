5e35aaee1b655f9f67cc75d6f10af92ddc61de1b
Joshua Williams <josh-131313@hotmail.com>
2019-03-24 09:44:43 -0700

    Update SubRegister.java

--- src/main/java/fortville/opcodes/SubRegister.java
+++ src/main/java/fortville/opcodes/SubRegister.java
@@ -11,6 +11,21 @@ import fortville.components.Registers;
 public class SubRegister implements Opcode{
     @Override
     public void execute(short data1, short data2, short data3, Memory memory, Display display, Registers registers){
-        
+        /*8xy5 - SUB Vx, Vy
+		Set Vx = Vx - Vy, set VF = NOT borrow.
+		If Vx > Vy, then VF is set to 1, otherwise 0. Then Vy is subtracted from Vx, and the results stored in Vx.*/
+		int VX = (0xff & (registers.loadRegister(data1)));
+		int VY = (0xff & (registers.loadRegister(data2)));
+		if(VX > VY){
+			registers.storeRegister((short) 15, (byte) 1);
+		}else{
+			registers.storeRegister((short) 15, (byte) 0);
+		}
+
+		VX = ((VX - VY) & 0xff);
+		registers.storeRegister(data1, (byte) VX);
+
+		//Do we want incrementPC in opcode execution?
+		registers.incrementPC();
     }
 }
