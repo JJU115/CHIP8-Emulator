bc1ef8ecfc73e1e6b3957b8154e55f8bde7d5af8
Chris Hettrick <git@structfoo.com>
2019-03-28 15:57:54 -0700

    Implement VectorLoadRegister instruction. Add two test cases.

--- src/main/java/fortville/opcodes/VectorLoadRegister.java
+++ src/main/java/fortville/opcodes/VectorLoadRegister.java
@@ -12,6 +12,18 @@ public class VectorLoadRegister implements Opcode {
     @Override
     public void execute(short data1, short data2, short data3,
         Memory memory, Display display, Registers registers) {
+        /*
+         * Fx65 - LD Vx, [I]
+         * Read registers V0 through Vx from memory starting at location I.
+         * The interpreter reads values from memory starting at location I
+         * into registers V0 through Vx.
+         */
+        short VX = data1;
+        short addrOfI = registers.loadI(); // Get memory address from I.
 
+        for (short regNum = 0; regNum <= VX; regNum++) {
+            byte data = memory.load(addrOfI + regNum);
+            registers.storeRegister(regNum, data);
+        }
     }
 }
--- src/test/java/fortville/opcodes/OpcodesTest.java
+++ src/test/java/fortville/opcodes/OpcodesTest.java
@@ -89,4 +89,54 @@ public class OpcodesTest {
         assertEquals(memory.load(addrI + 1), 4);
         assertEquals(memory.load(addrI + 2), 1);
     }
+
+    @Test
+    public void testVectorLoadRegister_V0() {
+        byte initValue = 42;
+        byte testValue;
+        short regVX = 0;
+        short addrI = 0x200;
+
+        // Given
+        registers.storeI(addrI); // Set register I to addrI.
+        testValue = memory.load(registers.loadI()); // Save value from [addrI].
+        registers.storeRegister(regVX, initValue); // Store init value.
+
+        assertEquals(registers.loadRegister(regVX), initValue);
+        assertNotEquals(registers.loadRegister(regVX), testValue);
+
+        // When
+        VectorLoadRegister op = new VectorLoadRegister();
+        op.execute(regVX, zero, zero, memory, display, registers);
+
+        // Then
+        assertEquals(registers.loadRegister(regVX), testValue);
+    }
+
+    @Test
+    public void testVectorLoadRegister_V4() {
+        byte initValue = 42;
+        short regVX = 4;
+        short addrI = 0x200;
+
+        // Given
+        registers.storeI(addrI); // Set register I to addrI.
+        for (short regNum = 0; regNum <= regVX; regNum++) {
+            registers.storeRegister(regNum, initValue); // Store init values.
+        }
+
+        for (short regNum = 0; regNum <= regVX; regNum++) { // Check init values.
+            assertEquals(registers.loadRegister(regNum), initValue);
+            assertNotEquals(registers.loadRegister(regNum), memory.load(addrI + regNum));
+        }
+
+        // When
+        VectorLoadRegister op = new VectorLoadRegister();
+        op.execute(regVX, zero, zero, memory, display, registers);
+
+        // Then
+        for (short regNum = 0; regNum <= regVX; regNum++) {
+            assertEquals(registers.loadRegister(regNum), memory.load(addrI + regNum));
+        }
+    }
 }
