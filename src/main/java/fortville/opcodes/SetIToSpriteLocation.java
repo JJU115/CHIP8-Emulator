e3ffc209c03f56b906d3f5f50604b0372cd2b11c
Justin Underhay <jju115@telus.net>
2019-03-27 17:17:07 -0700

    Implement SetIToSpriteLocation instruction

--- src/main/java/fortville/opcodes/SetIToSpriteLocation.java
+++ src/main/java/fortville/opcodes/SetIToSpriteLocation.java
@@ -7,11 +7,14 @@ import fortville.interfaces.Opcode;
 
 /**
  * SetIToSpriteLocation
+ * Fx29 - LD F, Vx
+ * Set I = location of sprite for digit Vx.
  */
 public class SetIToSpriteLocation implements Opcode {
     @Override
     public void execute(short data1, short data2, short data3,
         Memory memory, Display display, Registers registers) {
 
+        registers.storeI(5*registers.loadRegister(data1));
     }
 }
