3e72dff4f54e35a2eac4e583ef8274f7e6438d13
Chris Hettrick <git@structfoo.com>
2019-03-28 01:36:14 -0700

    Implement StoreBCDToMemory instruction. Add test cases.
    Bytes are signed in Java, so care is needed to not get negative values.
    
    Two straightforward test cases show the 'Given, When, Then' testing approach.
    First setup, then opcode execute, then confirmation of action.

--- src/main/java/fortville/opcodes/StoreBCDToMemory.java
+++ src/main/java/fortville/opcodes/StoreBCDToMemory.java
@@ -12,6 +12,26 @@ public class StoreBCDToMemory implements Opcode {
     @Override
     public void execute(short data1, short data2, short data3,
         Memory memory, Display display, Registers registers) {
+        /*
+         * Fx33 - LD B, Vx
+         * Store BCD representation of Vx in memory locations I, I+1, and I+2.
+         * The interpreter takes the decimal value of Vx,
+         * and places the hundreds digit in memory at location in I,
+         * the tens digit at location I+1,
+         * and the ones digit at location I+2.
+         */
+        // Read value from Vx. It is in data1. Watch out for signed bytes.
+        int bcd = Byte.toUnsignedInt(registers.loadRegister(data1));
 
+        byte hundreds = (byte)(bcd / 100);
+        bcd %= 100;
+        byte tens = (byte)(bcd / 10);
+        bcd %= 10;
+        byte ones = (byte)bcd;
+
+        short addrOfI = registers.loadI(); // Get memory address from I.
+        memory.store(hundreds, addrOfI);
+        memory.store(tens, addrOfI + 1);
+        memory.store(ones, addrOfI + 2);
     }
 }
--- src/test/java/fortville/opcodes/OpcodesTest.java
+++ src/test/java/fortville/opcodes/OpcodesTest.java
@@ -1,7 +1,13 @@
 package fortville.opcodes;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
+
+import fortville.components.Display;
+import fortville.components.Memory;
+import fortville.components.Registers;
+import fortville.opcodes.StoreBCDToMemory;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -9,11 +15,21 @@ import org.junit.Test;
 public class OpcodesTest {
 
     private int value;
+    private short zero;
+
+    Registers registers;
+    Memory memory;
+    Display display;
 
     @Before
     public void initOpcodesTest() {
         // XXX Common setup for all tests.
         value = 42;
+        zero = 0;
+
+        registers = new Registers();
+        memory = new Memory("input.ch8", registers); // XXX A Test rom would be nice.
+        display = new Display();
     }
 
     @Test
@@ -24,5 +40,47 @@ public class OpcodesTest {
     @Test
     public void testJumpAddress() {
         assertTrue("XXX Testing", true);
+    }
+
+    @Test
+    public void testStoreBCDToMemory_SmallByte() {
+        byte testValue = 35; // Small value, so always positive.
+        short regV1 = 1;
+        short addrI = 0x200;
+
+        registers.storeI(addrI);
+        registers.storeRegister(regV1, testValue);
+
+        assertNotEquals(memory.load(addrI), 0);
+        assertNotEquals(memory.load(addrI + 1), 3);
+        assertNotEquals(memory.load(addrI + 2), 5);
+
+        StoreBCDToMemory op = new StoreBCDToMemory();
+        op.execute(regV1, zero, zero, memory, display, registers);
+
+        assertEquals(memory.load(addrI), 0);
+        assertEquals(memory.load(addrI + 1), 3);
+        assertEquals(memory.load(addrI + 2), 5);
+    }
+
+    @Test
+    public void testStoreBCDToMemory_BigByte() {
+        byte testValue = (byte)241; // Big value, so could be negative if signed.
+        short regV1 = 1;
+        short addrI = 0x200;
+
+        registers.storeI(addrI);
+        registers.storeRegister(regV1, testValue);
+
+        assertNotEquals(memory.load(addrI), 2);
+        assertNotEquals(memory.load(addrI + 1), 4);
+        assertNotEquals(memory.load(addrI + 2), 1);
+
+        StoreBCDToMemory op = new StoreBCDToMemory();
+        op.execute(regV1, zero, zero, memory, display, registers);
+
+        assertEquals(memory.load(addrI), 2);
+        assertEquals(memory.load(addrI + 1), 4);
+        assertEquals(memory.load(addrI + 2), 1);
     }
 }
