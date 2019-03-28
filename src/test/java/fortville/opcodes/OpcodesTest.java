21b6190c10b3d0b4bbf57d2ab100f00cc42b6bcd
Chris Hettrick <git@structfoo.com>
2019-03-28 16:02:18 -0700

    Remove two simple example tests as we now have real tests for insight.
    Sort tests lexicographically by method name.

--- src/test/java/fortville/opcodes/OpcodesTest.java
+++ src/test/java/fortville/opcodes/OpcodesTest.java
@@ -14,7 +14,6 @@ import org.junit.Test;
 
 public class OpcodesTest {
 
-    private int value;
     private short zero;
 
     Registers registers;
@@ -23,47 +22,12 @@ public class OpcodesTest {
 
     @Before
     public void initOpcodesTest() {
-        // XXX Common setup for all tests.
-        value = 42;
+        // Common setup for all tests.
         zero = 0;
 
         registers = new Registers();
         memory = new Memory("input.ch8", registers); // XXX A Test rom would be nice.
         display = new Display();
-    }
-
-    @Test
-    public void testAddConstToRegister() {
-        assertEquals(value, 42);
-    }
-
-    @Test
-    public void testJumpAddress() {
-        assertTrue("XXX Testing", true);
-    }
-
-    @Test
-    public void testStoreBCDToMemory_SmallByte() {
-        byte testValue = 35; // Small value, so always positive.
-        short regV1 = 1;
-        short addrI = 0x200;
-
-        // Given
-        registers.storeI(addrI);
-        registers.storeRegister(regV1, testValue);
-
-        assertNotEquals(memory.load(addrI), 0);
-        assertNotEquals(memory.load(addrI + 1), 3);
-        assertNotEquals(memory.load(addrI + 2), 5);
-
-        // When
-        StoreBCDToMemory op = new StoreBCDToMemory();
-        op.execute(regV1, zero, zero, memory, display, registers);
-
-        // Then
-        assertEquals(memory.load(addrI), 0);
-        assertEquals(memory.load(addrI + 1), 3);
-        assertEquals(memory.load(addrI + 2), 5);
     }
 
     @Test
@@ -88,6 +52,30 @@ public class OpcodesTest {
         assertEquals(memory.load(addrI), 2);
         assertEquals(memory.load(addrI + 1), 4);
         assertEquals(memory.load(addrI + 2), 1);
+    }
+
+    @Test
+    public void testStoreBCDToMemory_SmallByte() {
+        byte testValue = 35; // Small value, so always positive.
+        short regV1 = 1;
+        short addrI = 0x200;
+
+        // Given
+        registers.storeI(addrI);
+        registers.storeRegister(regV1, testValue);
+
+        assertNotEquals(memory.load(addrI), 0);
+        assertNotEquals(memory.load(addrI + 1), 3);
+        assertNotEquals(memory.load(addrI + 2), 5);
+
+        // When
+        StoreBCDToMemory op = new StoreBCDToMemory();
+        op.execute(regV1, zero, zero, memory, display, registers);
+
+        // Then
+        assertEquals(memory.load(addrI), 0);
+        assertEquals(memory.load(addrI + 1), 3);
+        assertEquals(memory.load(addrI + 2), 5);
     }
 
     @Test
