2be3199b7637940127873e0b79b20d78f9e6ba27
Chris Hettrick <git@structfoo.com>
2019-03-31 16:08:59 -0700

    Fix formatting. Change tabs to 4 spaces. Checkstyle now passes.

--- src/main/java/fortville/components/Keyboard.java
+++ src/main/java/fortville/components/Keyboard.java
@@ -6,30 +6,34 @@ import java.awt.event.KeyListener;
 import java.util.Arrays;
 
 /**
- * Keyboard Original key mappings mapped to 1 | 2 | 3 | 4 Q | W | E | R A | S |
- * D | F Z | X | C | V
+ * Keyboard
+ * Original key mappings mapped to
+ * 1 | 2 | 3 | 4
+ * Q | W | E | R
+ * A | S | D | F
+ * Z | X | C | V
  */
 public class Keyboard extends KeyAdapter {
 
-	boolean[] keys = new boolean[16];
-	boolean keyDown = false;
-	int lastKey;
+    boolean[] keys = new boolean[16];
+    boolean keyDown = false;
+    int lastKey;
 
-	/**
-	 * @return the keys
-	 */
-	public boolean[] getKeys() {
-		return keys;
-	}
+    /**
+     * @return the keys
+     */
+    public boolean[] getKeys() {
+        return keys;
+    }
 
-	public int waitForKey() {
-		keyDown = false;
-		while (!keyDown) {
-			try {
-				Thread.sleep(20);
-			} catch (InterruptedException e) {
-				// TODO Handle Error
-			}
+    public int waitForKey() {
+        keyDown = false;
+        while (!keyDown) {
+            try {
+                Thread.sleep(20);
+            } catch (InterruptedException e) {
+                // TODO Handle Error
+            }
         }
         return lastKey;
     }
@@ -38,126 +42,126 @@ public class Keyboard extends KeyAdapter {
     public void keyPressed(KeyEvent e) {
         keyDown = true;
         switch (e.getKeyCode()) {
-			case KeyEvent.VK_X:
-				keys[0] = true;
-				lastKey = 0;
-				break;
-			case KeyEvent.VK_1:
-				keys[1] = true;
-				lastKey = 1;
-				break;
-			case KeyEvent.VK_2:
-				keys[2] = true;
-				lastKey = 2;
-				break;
-			case KeyEvent.VK_3:
-				keys[3] = true;
-				lastKey = 3;
-				break;
-			case KeyEvent.VK_Q:
-				keys[4] = true;
-				lastKey = 4;
-				break;
-			case KeyEvent.VK_W:
-				keys[5] = true;
-				lastKey = 5;
-				break;
-			case KeyEvent.VK_E:
-				keys[6] = true;
-				lastKey = 6;
-				break;
-			case KeyEvent.VK_A:
-				keys[7] = true;
-				lastKey = 7;
-				break;
-			case KeyEvent.VK_S:
-				keys[8] = true;
-				lastKey = 8;
-				break;
-			case KeyEvent.VK_D:
-				keys[9] = true;
-				lastKey = 9;
-				break;
-			case KeyEvent.VK_Z:
-				keys[10] = true;
-				lastKey = 10;
-				break;
-			case KeyEvent.VK_C:
-				keys[11] = true;
-				lastKey = 11;
-				break;
-			case KeyEvent.VK_4:
-				keys[12] = true;
-				lastKey = 12;
-				break;
-			case KeyEvent.VK_R:
-				keys[13] = true;
-				lastKey = 13;
-				break;
-			case KeyEvent.VK_F:
-				keys[14] = true;
-				lastKey = 14;
-				break;
-			case KeyEvent.VK_V:
-				keys[15] = true;
-				lastKey = 15;
-				break;
-			default:
+        case KeyEvent.VK_X:
+            keys[0] = true;
+            lastKey = 0;
+            break;
+        case KeyEvent.VK_1:
+            keys[1] = true;
+            lastKey = 1;
+            break;
+        case KeyEvent.VK_2:
+            keys[2] = true;
+            lastKey = 2;
+            break;
+        case KeyEvent.VK_3:
+            keys[3] = true;
+            lastKey = 3;
+            break;
+        case KeyEvent.VK_Q:
+            keys[4] = true;
+            lastKey = 4;
+            break;
+        case KeyEvent.VK_W:
+            keys[5] = true;
+            lastKey = 5;
+            break;
+        case KeyEvent.VK_E:
+            keys[6] = true;
+            lastKey = 6;
+            break;
+        case KeyEvent.VK_A:
+            keys[7] = true;
+            lastKey = 7;
+            break;
+        case KeyEvent.VK_S:
+            keys[8] = true;
+            lastKey = 8;
+            break;
+        case KeyEvent.VK_D:
+            keys[9] = true;
+            lastKey = 9;
+            break;
+        case KeyEvent.VK_Z:
+            keys[10] = true;
+            lastKey = 10;
+            break;
+        case KeyEvent.VK_C:
+            keys[11] = true;
+            lastKey = 11;
+            break;
+        case KeyEvent.VK_4:
+            keys[12] = true;
+            lastKey = 12;
+            break;
+        case KeyEvent.VK_R:
+            keys[13] = true;
+            lastKey = 13;
+            break;
+        case KeyEvent.VK_F:
+            keys[14] = true;
+            lastKey = 14;
+            break;
+        case KeyEvent.VK_V:
+            keys[15] = true;
+            lastKey = 15;
+            break;
+        default:
         }
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
         switch (e.getKeyCode()) {
-			case KeyEvent.VK_X:
-				keys[0] = false;
-				break;
-			case KeyEvent.VK_1:
-				keys[1] = false;
-				break;
-			case KeyEvent.VK_2:
-				keys[2] = false;
-				break;
-			case KeyEvent.VK_3:
-				keys[3] = false;
-				break;
-			case KeyEvent.VK_Q:
-				keys[4] = false;
-				break;
-			case KeyEvent.VK_W:
-				keys[5] = false;
-				break;
-			case KeyEvent.VK_E:
-				keys[6] = false;
-				break;
-			case KeyEvent.VK_A:
-				keys[7] = false;
-				break;
-			case KeyEvent.VK_S:
-				keys[8] = false;
-				break;
-			case KeyEvent.VK_D:
-				keys[9] = false;
-				break;
-			case KeyEvent.VK_Z:
-				keys[10] = false;
-				break;
-			case KeyEvent.VK_C:
-				keys[11] = false;
-				break;
-			case KeyEvent.VK_4:
-				keys[12] = false;
-				break;
-			case KeyEvent.VK_R:
-				keys[13] = false;
-				break;
-			case KeyEvent.VK_F:
-				keys[14] = false;
-				break;
-			case KeyEvent.VK_V:
-				keys[15] = false;
-				break;
-			default:
+        case KeyEvent.VK_X:
+            keys[0] = false;
+            break;
+        case KeyEvent.VK_1:
+            keys[1] = false;
+            break;
+        case KeyEvent.VK_2:
+            keys[2] = false;
+            break;
+        case KeyEvent.VK_3:
+            keys[3] = false;
+            break;
+        case KeyEvent.VK_Q:
+            keys[4] = false;
+            break;
+        case KeyEvent.VK_W:
+            keys[5] = false;
+            break;
+        case KeyEvent.VK_E:
+            keys[6] = false;
+            break;
+        case KeyEvent.VK_A:
+            keys[7] = false;
+            break;
+        case KeyEvent.VK_S:
+            keys[8] = false;
+            break;
+        case KeyEvent.VK_D:
+            keys[9] = false;
+            break;
+        case KeyEvent.VK_Z:
+            keys[10] = false;
+            break;
+        case KeyEvent.VK_C:
+            keys[11] = false;
+            break;
+        case KeyEvent.VK_4:
+            keys[12] = false;
+            break;
+        case KeyEvent.VK_R:
+            keys[13] = false;
+            break;
+        case KeyEvent.VK_F:
+            keys[14] = false;
+            break;
+        case KeyEvent.VK_V:
+            keys[15] = false;
+            break;
+        default:
         }
     }
 }
